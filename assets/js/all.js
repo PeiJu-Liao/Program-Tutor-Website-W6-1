4b89ca59ac01e796d7faa5db1f31c0bbdbb375dd
PeiJu-Liao <peggy1996850519@gmail.com>
2022-08-30 16:11:19 +0800

    Update 2022-08-30T08:11:15.499Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,63 +1,62 @@
-const swiper = new Swiper(".courseSwiper", {
+"use strict";
+
+var swiper = new Swiper(".courseSwiper", {
   slidesPerView: 3,
   spaceBetween: 20,
   loop: true,
   autoplay: {
     delay: 2500,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   breakpoints: {
     // when window width is >= 576px
     576: {
       slidesPerView: 3,
-      spaceBetween: 20,
+      spaceBetween: 20
     },
     768: {
       slidesPerView: 4,
-      spaceBetween: 30,
+      spaceBetween: 30
     },
     992: {
       slidesPerView: 5,
-      spaceBetween: 16,
+      spaceBetween: 16
     },
     1200: {
       slidesPerView: 7,
-      spaceBetween: 16,
+      spaceBetween: 16
     }
   }
 });
-
-
-const swiper2 = new Swiper(".tutorial-swiper", {
+var swiper2 = new Swiper(".tutorial-swiper", {
   slidesPerView: 2,
   spaceBetween: 24,
   loop: true,
   autoplay: {
     delay: 2500,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   breakpoints: {
     // when window width is >= 576px
     576: {
       slidesPerView: 2,
-      spaceBetween: 24,
+      spaceBetween: 24
     },
     768: {
       slidesPerView: 3,
-      spaceBetween: 24,
+      spaceBetween: 24
     },
     992: {
       slidesPerView: 4,
-      spaceBetween: 16,
+      spaceBetween: 16
     },
     1200: {
       slidesPerView: 5,
-      spaceBetween: 16,
+      spaceBetween: 16
     }
   }
 });
-
-const swiper3 = new Swiper(".feedback-swiper", {
+var swiper3 = new Swiper(".feedback-swiper", {
   effect: 'fade',
   fadeEffect: {
     crossFade: true
@@ -65,18 +64,17 @@ const swiper3 = new Swiper(".feedback-swiper", {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
+    clickable: true
   },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-})
-
-const elem = document.querySelector('input[name="datepicker"]');
-const datepicker = new Datepicker(elem, {
+    prevEl: '.swiper-button-prev'
+  }
+});
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
   language: "zh-TW",
   clearBtn: true
 });
-
+//# sourceMappingURL=all.js.map
